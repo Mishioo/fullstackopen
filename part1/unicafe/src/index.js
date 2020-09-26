936ada4b0fd2c70534b171d4e73356c8ab9a12ee
Michał Więcław <wieclawmm@gmail.com>
2020-09-26 14:38:57 +0200

    ex. 1.6.-1.10.

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Button = ({ text, counter, setCounter }) => (
+  <button onClick={()=>setCounter(counter + 1)}>{text}</button>
+  )
+
+const Statistic = ({ text, counter, unit }) => {
+  if (Number.isNaN(counter)) counter = 0
+  counter = Math.round((counter + Number.EPSILON) * 100) / 100
+  return <p>{text}: {counter} {unit || ''}</p>
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <Statistic text='good' counter={good} />
+      <Statistic text='neutral' counter={neutral} />
+      <Statistic text='bad' counter={bad} />
+      <Statistic text='all' counter={all} />
+      <Statistic text='average' counter={ (good - bad) / all } />
+      <Statistic text='positive' counter={ good * 100 / all } unit='%' />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' counter={good} setCounter={setGood} />
+      <Button text='neutral' counter={neutral} setCounter={setNeutral} />
+      <Button text='bad' counter={bad} setCounter={setBad} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
