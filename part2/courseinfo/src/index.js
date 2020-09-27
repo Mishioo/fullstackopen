46bb3f8db1bc2aacbfd80ed86c3b319f179de604
Michał Więcław <wieclawmm@gmail.com>
2020-09-27 12:10:16 +0200

    ex. 2.5.

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,48 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((s, part) => s + part.exercises, 0)
-  return(
-    <p><b>total of {sum} exercises</b></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
-
-const Courses = ({ courses }) => {
-  return <div>{courses.map(c => <Course key={c.id} course={c} />)}</div>
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -90,7 +48,7 @@ const App = () => {
     }
   ]
 
-  return <Courses courses={courses} />
+  return <div>{courses.map(c => <Course key={c.id} course={c} />)}</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
