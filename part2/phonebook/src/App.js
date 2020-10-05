375739eb36600a960950441f1e9c3e6a54f4af77
Michał Więcław <wieclawmm@gmail.com>
2020-10-05 10:30:19 +0200

    ex. 2.19.-2.20.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import { InfoNotification, ErrorNotification } from './components/Notifications'
 import personsService from './services/persons'
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [ errorMessage, setErrorMessage ] = useState(null)
+  const [ infoMessage, setInfoMessage ] = useState(null)
 
   useEffect(() => {
     personsService
@@ -18,8 +21,6 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    // use existing person to preserve other fields
-    // this implementation breaks when deleted after update
     const newPerson = { name: newName, number:newNumber }
     const personExists = persons.map(p => p.name).includes(newName)
     if (
@@ -35,12 +36,14 @@ const App = () => {
           person => setPersons(persons.map(
             p => p.id !== person.id ? p : {...p, number:newNumber}
           ))
+        .then(() => {setInfoMessage(`Added ${newName} successfully.`)})
         )
-    } else if (personExists) {
-      return
-    } else {
-      personsService.create(newPerson)
+      } else if (personExists) {
+        return
+      } else {
+        personsService.create(newPerson)
         .then(person => setPersons(persons.concat(person)))
+        .then(() => {setInfoMessage(`Added ${newName} successfully.`)})
     }
     setNewName('')
     setNewNumber('')
@@ -54,12 +57,20 @@ const App = () => {
       .then(() => {
         setPersons(persons.filter(p => p.id !== person.id))
       })
-      .catch(error => {console.log('error data', error)})
+      .then(() => {setInfoMessage(`Deleted ${person.name} successfully.`)})
+      .catch(() => {
+        setErrorMessage(`${person.name} already deleted.`)
+        personsService
+          .getAll()
+          .then(data => setPersons(data))
+      })
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <ErrorNotification message={errorMessage} setter={setErrorMessage}/>
+      <InfoNotification message={infoMessage} setter={setInfoMessage}/>
       <Filter filter={filter} setFilter={setFilter}/>
       <h3>Addd new entry</h3>
       <PersonForm
