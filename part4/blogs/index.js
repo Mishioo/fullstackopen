207c3b8d0ea9332dda0183db107b189cb7093580
Michał M. Więcław <wieclawmm@gmail.com>
2020-10-25 13:59:14 +0100

    ex. 4.2. models

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -2,17 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
+const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(
   config.MONGODB_URI,
