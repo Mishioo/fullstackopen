03768c8db9bfd987b923075ea067976a4dceb205
Michał M. Więcław <wieclawmm@gmail.com>
2020-10-25 14:10:08 +0100

    ex. 4.2. app

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -1,23 +1,9 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const config = require('./utils/config')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
 
-mongoose.connect(
-  config.MONGODB_URI,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true }
-)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
