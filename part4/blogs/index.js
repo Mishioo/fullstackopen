c6e7fe3ec4223ab39bb659a5aad62b030384a980
Michał M. Więcław <wieclawmm@gmail.com>
2020-10-25 14:04:23 +0100

    ex. 4.2. controllers

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -4,8 +4,7 @@ const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(
   config.MONGODB_URI,
@@ -17,24 +16,7 @@ mongoose.connect(
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
