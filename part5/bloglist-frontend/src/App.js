f36e247cfb967b7cf18bed0aa525a59641ccf255
Michał Więcław <wieclawmm@gmail.com>
2020-12-16 12:31:46 +0100

    ex. 5.1.

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,7 +18,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {user === null ?
+        LoginForm({username, setUsername, password, setPassword, setUser}) :
+        <p>{`${user.name} logged in`}</p>}
+      {user !== null && blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
