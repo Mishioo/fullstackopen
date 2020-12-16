14f1eaf571bcc9d9d6dc9e4abfb83fbca4e7280e
Michał Więcław <wieclawmm@gmail.com>
2020-12-16 22:53:01 +0100

    ex. 5.10.

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -5,9 +5,9 @@ const BlogList = ({ user, blogs, setBlogs }) => {
   return (
     <dir>
       {blogs
-          .filter(blog => blog.user.username === user.username)
-          .sort((a, b) => (b.likes - a.likes))
-          .map(blog => <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />)}
+        .filter(blog => blog.user.username === user.username)
+        .sort((a, b) => (b.likes - a.likes))
+        .map(blog => <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />)}
     </dir>
   )
 }
